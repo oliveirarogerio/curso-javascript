88bf4d8efbce27053ff111373b9abe18e4f38820
Rogério Oliveira <oliveirarogerio35@gmail.com>
2020-04-01 11:33:31 -0400

    Função

---
+++ exercicios-js/funcao/tiposDeclaracao.js
@@ -0,0 +1,19 @@
+console.log(soma(3, 4));
+//console.log(sub(3, 4)); sub is not defined
+
+// function declaration
+function soma(x,y){
+    return x + y
+}
+
+//function expression
+const sub = function(x,y){
+    return x - y
+}
+
+//named function expression
+
+const mult = function mult(x,y){
+    return x * y
+}
+
